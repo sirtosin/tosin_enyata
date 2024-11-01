f05944cd6bdd71f42af8e76769cde26d162be033
sirtosin <sirtosin45@gmail.com>
2024-11-01 07:51:37 +0100

    build err fix

--- components/Details.tsx
+++ components/Details.tsx
@@ -2,19 +2,21 @@ import React from "react";
 
 export default function Details({ data, type }: any) {
   return (
-    <div className="p-10">
-      <section className="flex space-x-6">
-        <div className="">
+    <div className="p-5 sm:p-10">
+      <section className="flex flex-col-reverse space-y-5 sm:space-y-0 sm:flex-row sm:space-x-6">
+        <div className="mt-5 sm:mt-0">
           <img
             className="rounded-lg w-[400px] object-cover h-[450px]"
             src="https://assets-us-01.kc-usercontent.com/5cb25086-82d2-4c89-94f0-8450813a0fd3/0c3fcefb-bc28-4af6-985e-0c3b499ae832/Elon_Musk_Royal_Society.jpg?fm=jpg&auto=format"
             alt=""
           />
         </div>
-        {type === "film" && <GenreFilmType data={data} />}
-        {type === "people" && <GenrePeopleType data={data} />}
-        {type === "starship" && <GenreStarType data={data} />}
-        {type === "species" && <GenreSpeciesType data={data} />}
+        <div>
+          {type === "film" && <GenreFilmType data={data} />}
+          {type === "people" && <GenrePeopleType data={data} />}
+          {type === "starship" && <GenreStarType data={data} />}
+          {type === "species" && <GenreSpeciesType data={data} />}
+        </div>
       </section>
     </div>
   );
